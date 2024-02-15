54c228228aff5bd3cd9717efebd355792922c856
Krista Stadler <kristastadler@Kristas-MacBook-Prolocal>
2024-02-14 19:54:04 -0700

    remove useless font in global css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-roboto font-bold text-center">
+    <h1 className="text-3xl font-bold text-center">
       Hello world!
     </h1>
   )
